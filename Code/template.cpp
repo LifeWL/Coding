31d8c9ea7094abe223ab47cd12a406e5ffc119b9
weixuan <951254767@qq.com>
2022-11-07 23:10:46 +0800

    update

--- Code/template.cpp
+++ Code/template.cpp
@@ -92,3 +92,25 @@ void remove(int k)
 {
     ne[k] = ne[ne[k]];
 }
+
+//双向链表
+
+int e[N], l[N], r[N], idx;
+
+void init()
+{
+    r[0] = 1, l[1] = 0;
+}
+
+void insert(int k, int x)
+{
+    e[idx] = x;
+    l[idx] = k, r[idx] = r[k];
+    l[r[k]] = idx, r[k] = idx++;
+}
+
+void remove(int k)
+{
+    l[r[k]] = l[k];
+    r[l[k]] = r[k]; 
+}
