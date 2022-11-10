9f8555eaf59e2b1f22be355bcde64baf15b0154a
weixuan <951254767@qq.com>
2022-11-10 22:50:05 +0800

    update

--- Code/template.cpp
+++ Code/template.cpp
@@ -168,3 +168,22 @@ for (int i = 0; i < n; i ++ )
     while (hh <= tt && check(q[tt], i)) tt -- ;
     q[ ++ tt] = i;
 }
+
+//KMP
+//s[]是长文本，p[]是模式串，n是s的长度，m是p的长度
+for (int i = 2, j = 0; i <= m; i++)
+{
+    while (j && p[i] != p[j + 1]) j = ne[j];
+    if (p[i] == p[j + 1]) j++;
+    ne[i] = j;
+}
+
+for (int i = 1, j = 0; i <= n; i++)
+{
+    while (j && s[i] != p[j + 1]) j = ne[j];
+    if (s[i] == p[j + 1]) j++;
+    if (j == m)
+    {
+        j = ne[j];
+    }
+}
