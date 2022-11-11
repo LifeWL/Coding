4d6ac32fcaa9cde5835d295401b0ce7309e9bb31
weixuan <951254767@qq.com>
2022-11-11 23:54:42 +0800

    update

--- Code/template.cpp
+++ Code/template.cpp
@@ -187,3 +187,30 @@ for (int i = 1, j = 0; i <= n; i++)
         j = ne[j];
     }
 }
+
+//Trie树
+int son[N][26], cnt[N], idx;
+
+void insert(char *str)
+{
+    int p = 0;
+    for (int i = 0; str[i]; i ++ )
+    {
+        int u = str[i] - 'a';
+        if (!son[p][u]) son[p][u] = ++ idx;
+        p = son[p][u];
+    }
+    cnt[p] ++ ;
+}
+
+int query(char *str)
+{
+    int p = 0;
+    for (int i = 0; str[i]; i ++ )
+    {
+        int u = str[i] - 'a';
+        if (!son[p][u]) return 0;
+        p = son[p][u];
+    }
+    return cnt[p];
+}
