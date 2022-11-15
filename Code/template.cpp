7df03457b9edd8817479eb386ca18d0c343b8811
weixuan <951254767@qq.com>
2022-11-15 22:58:20 +0800

    update

--- Code/template.cpp
+++ Code/template.cpp
@@ -305,3 +305,27 @@ void up(int u)
 }
 
 for (int i = n / 2; i; i -- ) down(i);
+
+//拉链哈希
+int h[N], e[N], ne[N], idx;
+
+void insert(int x)
+{
+    int k = (x % N + N) % N;
+    e[idx] = x;
+    ne[idx] = h[k];
+    h[k] = idx++;
+}
+
+bool find(int x)
+{
+    int k = (x % N + N) % N;
+    for (int i = h[k]; i != -1; i = ne[i])
+    {
+        if (e[i] == x)
+        {
+            return true;
+        }
+    }
+    return false;
+}
