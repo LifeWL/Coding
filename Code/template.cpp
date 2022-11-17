c19fe28b6575cf5518ca629c532548efaba7fc8a
weixuan <951254767@qq.com>
2022-11-17 22:48:01 +0800

    update

--- Code/template.cpp
+++ Code/template.cpp
@@ -343,3 +343,19 @@ int find(int x)
     }
     return t;
 }
+
+//字典哈希
+typedef unsigned long long ULL;
+ULL h[N], p[N];
+P = 131;
+p[0] = 1;
+for (int i = 1; i <= n; ++i)
+{
+    h[i] = h[i - 1] * P + str[i];
+    p[i] = p[i - 1] * P;
+}
+
+ULL get(int l, int r)
+{
+    return h[r] - h[l - 1] * p[r - l + 1];
+}
