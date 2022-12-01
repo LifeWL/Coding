bdf93d6580976d7399bb22107e90dd1cfe893ae4
weixuan <951254767@qq.com>
2022-12-01 23:47:17 +0800

    update

--- Code/template.cpp
+++ Code/template.cpp
@@ -497,3 +497,16 @@ void add(int a, int b)
 // 初始化
 idx = 0;
 memset(h, -1, sizeof h);
+
+//深度优先遍历
+
+int dfs(int u)
+{
+    st[u] = true; // st[u] 表示点u已经被遍历过
+
+    for (int i = h[u]; i != -1; i = ne[i])
+    {
+        int j = e[i];
+        if (!st[j]) dfs(j);
+    }
+}
