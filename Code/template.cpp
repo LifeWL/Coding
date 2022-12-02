3b731ba4a34f4373ea5351f3879398f137f0cc60
weixuan <951254767@qq.com>
2022-12-02 23:30:05 +0800

    update

--- Code/template.cpp
+++ Code/template.cpp
@@ -510,3 +510,25 @@ int dfs(int u)
         if (!st[j]) dfs(j);
     }
 }
+
+
+//宽度优先遍历
+queue<int> q;
+st[1] = true; // 表示1号点已经被遍历过
+q.push(1);
+
+while (q.size())
+{
+    int t = q.front();
+    q.pop();
+
+    for (int i = h[t]; i != -1; i = ne[i])
+    {
+        int j = e[i];
+        if (!st[j])
+        {
+            st[j] = true; // 表示点j已经被遍历过
+            q.push(j);
+        }
+    }
+}
