e85f40d94932a2868caf3380f6afea251e4be54e
weixuan <951254767@qq.com>
2022-12-10 23:53:20 +0800

    update

--- Code/template.cpp
+++ Code/template.cpp
@@ -770,3 +770,35 @@ void floyd()
             for (int j = 1; j <= n; j ++ )
                 d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
 }
+
+//朴素版prim算法 —— 模板题 AcWing 858. Prim算法求最小生成树
+//时间复杂度是 O(n2+m)O(n2+m), nn 表示点数，mm 表示边数
+int n;      // n表示点数
+int g[N][N];        // 邻接矩阵，存储所有边
+int dist[N];        // 存储其他点到当前最小生成树的距离
+bool st[N];     // 存储每个点是否已经在生成树中
+
+
+// 如果图不连通，则返回INF(值是0x3f3f3f3f), 否则返回最小生成树的树边权重之和
+int prim()
+{
+    memset(dist, 0x3f, sizeof dist);
+
+    int res = 0;
+    for (int i = 0; i < n; i ++ )
+    {
+        int t = -1;
+        for (int j = 1; j <= n; j ++ )
+            if (!st[j] && (t == -1 || dist[t] > dist[j]))
+                t = j;
+
+        if (i && dist[t] == INF) return INF;
+
+        if (i) res += dist[t];
+        st[t] = true;
+
+        for (int j = 1; j <= n; j ++ ) dist[j] = min(dist[j], g[t][j]);
+    }
+
+    return res;
+}
