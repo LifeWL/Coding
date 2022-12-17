ff84966911f376d01ff858f374424941701b96f3
weixuan <951254767@qq.com>
2022-12-17 21:51:30 +0800

    Update template.cpp

--- Code/template.cpp
+++ Code/template.cpp
@@ -928,3 +928,17 @@ bool is_prime(int x)
             return false;
     return true;
 }
+
+//试除法分解质因数
+void divide(int x)
+{
+    for (int i = 2; i <= x / i; i ++ )
+        if (x % i == 0)
+        {
+            int s = 0;
+            while (x % i == 0) x /= i, s ++ ;
+            cout << i << ' ' << s << endl;
+        }
+    if (x > 1) cout << x << ' ' << 1 << endl;
+    cout << endl;
+}
