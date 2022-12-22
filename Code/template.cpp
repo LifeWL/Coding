c40999e0496aa690a38f87ae94aaa16308c6adfa
weixuan <951254767@qq.com>
2022-12-22 23:45:53 +0800

    update

--- Code/template.cpp
+++ Code/template.cpp
@@ -974,3 +974,17 @@ void get_primes(int n)
         }
     }
 }
+
+//试除法求所有约数
+vector<int> get_divisors(int x)
+{
+    vector<int> res;
+    for (int i = 1; i <= x / i; i ++ )
+        if (x % i == 0)
+        {
+            res.push_back(i);
+            if (i != x / i) res.push_back(x / i);
+        }
+    sort(res.begin(), res.end());
+    return res;
+}
