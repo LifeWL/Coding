eb9cf0d32d6a82df3b66d7edae50c71a8037dc61
weixuan <951254767@qq.com>
2022-12-29 22:41:35 +0800

    update

--- Code/template.cpp
+++ Code/template.cpp
@@ -1059,3 +1059,17 @@ int qmi(int m, int k, int p)
     }
     return res;
 }
+
+//扩展欧几里得算法
+// 求x, y，使得ax + by = gcd(a, b)
+int exgcd(int a, int b, int &x, int &y)
+{
+    if (!b)
+    {
+        x = 1; y = 0;
+        return a;
+    }
+    int d = exgcd(b, a % b, y, x);
+    y -= (a/b) * x;
+    return d;
+}
