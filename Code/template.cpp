6e3801c141156168e0734f15e4f0ee4f0c2ec937
weixuan <951254767@qq.com>
2023-01-01 23:21:47 +0800

    update

--- Code/template.cpp
+++ Code/template.cpp
@@ -1119,3 +1119,26 @@ for (int i = 0; i < N; i ++ )
     for (int j = 0; j <= i; j ++ )
         if (!j) c[i][j] = 1;
         else c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % mod;
+
+//通过预处理逆元的方式求组合数
+//首先预处理出所有阶乘取模的余数fact[N]，以及所有阶乘取模的逆元infact[N]
+//如果取模的数是质数，可以用费马小定理求逆元
+int qmi(int a, int k, int p)    // 快速幂模板
+{
+    int res = 1;
+    while (k)
+    {
+        if (k & 1) res = (LL)res * a % p;
+        a = (LL)a * a % p;
+        k >>= 1;
+    }
+    return res;
+}
+
+// 预处理阶乘的余数和阶乘逆元的余数
+fact[0] = infact[0] = 1;
+for (int i = 1; i < N; i ++ )
+{
+    fact[i] = (LL)fact[i - 1] * i % mod;
+    infact[i] = (LL)infact[i - 1] * qmi(i, mod - 2, mod) % mod;
+}
