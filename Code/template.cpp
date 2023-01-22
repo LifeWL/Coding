87f77828c69877bf80fd9df822cc29d89bc23239
weixuan <951254767@qq.com>
2023-01-22 23:41:24 +0800

    update

--- Code/template.cpp
+++ Code/template.cpp
@@ -2811,6 +2811,73 @@ int main()
     return 0;
 }
 
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+
+#define x first
+#define y second
+
+using namespace std;
+
+typedef pair<int, int> PII;
+const int N = 55;
+
+int n, m;
+PII q[N];
+int ans;
+
+int calc()
+{
+    int res = 0;
+    for (int i = 0; i < m; i ++ )
+    {
+        res += q[i].x + q[i].y;
+        if (i < n)
+        {
+            if (q[i].x == 10) res += q[i + 1].x + q[i + 1].y;
+            else if (q[i].x + q[i].y == 10)
+                res += q[i + 1].x;
+        }
+    }
+    ans = max(ans, res);
+    return res;
+}
+
+void simulate_anneal()
+{
+    for (double t = 1e4; t > 1e-4; t *= 0.99)
+    {
+        int a = rand() % m, b = rand() % m;
+        int x = calc();
+        swap(q[a], q[b]);
+        if (n + (q[n - 1].x == 10) == m)
+        {
+            int y = calc();
+            int delta = y - x;
+            if (exp(delta / t) < (double)rand() / RAND_MAX)
+                swap(q[a], q[b]);
+        }
+        else swap(q[a], q[b]);
+    }
+}
+
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i ++ ) cin >> q[i].x >> q[i].y;
+    if (q[n - 1].x == 10) m = n + 1, cin >> q[n].x >> q[n].y;
+    else m = n;
+
+    for (int i = 0; i < 100; i ++ ) simulate_anneal();
+
+    cout << ans << endl;
+    return 0;
+}
+
+
 //爬山
 #include <iostream>
 #include <cstring>
