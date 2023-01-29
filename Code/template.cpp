94394db752871b195d2c3ff6208c1a56d044bf3d
weixuan <951254767@qq.com>
2023-01-29 23:55:22 +0800

    update

--- Code/template.cpp
+++ Code/template.cpp
@@ -3026,3 +3026,74 @@ int main()
 //            (2) 有源汇上下界最大流
 //            (3) 有源汇上下界最小流
 //        1.7.3 多源汇最大流
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 1010, M = 20010, INF = 1e8;
+
+int n, m, S, T;
+int h[N], e[M], f[M], ne[M], idx;
+int q[N], d[N], pre[N];
+bool st[N];
+
+void add(int a, int b, int c)
+{
+    e[idx] = b, f[idx] = c, ne[idx] = h[a], h[a] = idx ++ ;
+    e[idx] = a, f[idx] = 0, ne[idx] = h[b], h[b] = idx ++ ;
+}
+
+bool bfs()
+{
+    int hh = 0, tt = 0;
+    memset(st, false, sizeof st);
+    q[0] = S, st[S] = true, d[S] = INF;
+    while (hh <= tt)
+    {
+        int t = q[hh ++ ];
+        for (int i = h[t]; ~i; i = ne[i])
+        {
+            int ver = e[i];
+            if (!st[ver] && f[i])
+            {
+                st[ver] = true;
+                d[ver] = min(d[t], f[i]);
+                pre[ver] = i;
+                if (ver == T) return true;
+                q[ ++ tt] = ver;
+            }
+        }
+    }
+    return false;
+}
+
+int EK()
+{
+    int r = 0;
+    while (bfs())
+    {
+        r += d[T];
+        for (int i = T; i != S; i = e[pre[i] ^ 1])
+            f[pre[i]] -= d[T], f[pre[i] ^ 1] += d[T];
+    }
+    return r;
+}
+
+int main()
+{
+    scanf("%d%d%d%d", &n, &m, &S, &T);
+    memset(h, -1, sizeof h);
+    while (m -- )
+    {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
+        add(a, b, c);
+    }
+
+    printf("%d\n", EK());
+
+    return 0;
+}
