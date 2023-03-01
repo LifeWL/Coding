bca88763a33cd33cd78d7b66e47de7c7526ff7af
魏煊 <49360064+W-xuan@users.noreply.github.com>
2023-03-01 19:56:26 +0800

    更新 template.cpp

--- Code/template.cpp
+++ Code/template.cpp
@@ -5517,3 +5517,113 @@ int main()
     printf("%d\n", tot - res);
     return 0;
 }
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 40010, M = 400010, INF = 1e8;
+
+int n, m, S, T;
+int h[N], e[M], f[M], ne[M], idx;
+int q[N], d[N], cur[N];
+bool g[210][210];
+
+int get(int x, int y)
+{
+    return (x - 1) * n + y;
+}
+
+void add(int a, int b, int c)
+{
+    e[idx] = b, f[idx] = c, ne[idx] = h[a], h[a] = idx ++ ;
+    e[idx] = a, f[idx] = 0, ne[idx] = h[b], h[b] = idx ++ ;
+}
+
+bool bfs()
+{
+    int hh = 0, tt = 0;
+    memset(d, -1, sizeof d);
+    q[0] = S, d[S] = 0, cur[S] = h[S];
+    while (hh <= tt)
+    {
+        int t = q[hh ++ ];
+        for (int i = h[t]; ~i; i = ne[i])
+        {
+            int ver = e[i];
+            if (d[ver] == -1 && f[i])
+            {
+                d[ver] = d[t] + 1;
+                cur[ver] = h[ver];
+                if (ver == T) return true;
+                q[ ++ tt] = ver;
+            }
+        }
+    }
+    return false;
+}
+
+int find(int u, int limit)
+{
+    if (u == T) return limit;
+    int flow = 0;
+    for (int i = cur[u]; ~i && flow < limit; i = ne[i])
+    {
+        cur[u] = i;
+        int ver = e[i];
+        if (d[ver] == d[u] + 1 && f[i])
+        {
+            int t = find(ver, min(f[i], limit - flow));
+            if (!t) d[ver] = -1;
+            f[i] -= t, f[i ^ 1] += t, flow += t;
+        }
+    }
+    return flow;
+}
+
+int dinic()
+{
+    int r = 0, flow;
+    while (bfs()) while (flow = find(S, INF)) r += flow;
+    return r;
+}
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+    S = 0, T = n * n + 1;
+    memset(h, -1, sizeof h);
+    while (m -- )
+    {
+        int x, y;
+        scanf("%d%d", &x, &y);
+        g[x][y] = true;
+    }
+
+    int dx[] = {-2, -1, 1, 2, 2, 1, -1, -2};
+    int dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
+
+    int tot = 0;
+    for (int i = 1; i <= n; i ++ )
+        for (int j = 1; j <= n; j ++ )
+        {
+            if (g[i][j]) continue;
+            if (i + j & 1)
+            {
+                add(S, get(i, j), 1);
+                for (int k = 0; k < 8; k ++ )
+                {
+                    int x = i + dx[k], y = j + dy[k];
+                    if (x >= 1 && x <= n && y >= 1 && y <= n && !g[x][y])
+                        add(get(i, j), get(x, y), INF);
+                }
+            }
+            else add(get(i, j), T, 1);
+            tot ++ ;
+        }
+
+    printf("%d\n", tot - dinic());
+    return 0;
+}
