9970fb48ce01f85fff66e9eb85d351ed43ec99ce
魏煊 <49360064+W-xuan@users.noreply.github.com>
2023-03-28 00:23:47 +0800

    Update template.cpp

--- Code/template.cpp
+++ Code/template.cpp
@@ -7800,3 +7800,74 @@ int main()
 
     return 0;
 }
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long LL;
+const int N = 1000010;
+
+int n;
+int v[N], dist[N], l[N], r[N];
+struct Segment
+{
+    int end, root, size;
+}stk[N];
+int ans[N];
+
+int merge(int x, int y)
+{
+    if (!x || !y) return x + y;
+    if (v[x] < v[y]) swap(x, y);
+    r[x] = merge(r[x], y);
+    if (dist[r[x]] > dist[l[x]]) swap(r[x], l[x]);
+    dist[x] = dist[r[x]] + 1;
+    return x;
+}
+
+int pop(int x)
+{
+    return merge(l[x], r[x]);
+}
+
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i ++ )
+    {
+        scanf("%d", &v[i]);
+        v[i] -= i;
+    }
+    int tt = 0;
+    for (int i = 1; i <= n; i ++ )
+    {
+        auto cur = Segment({i, i, 1});
+        dist[i] = 1;
+        while (tt && v[cur.root] < v[stk[tt].root])
+        {
+            cur.root = merge(cur.root, stk[tt].root);
+            if (cur.size % 2 && stk[tt].size % 2)
+                cur.root = pop(cur.root);
+            cur.size += stk[tt].size;
+            tt -- ;
+        }
+        stk[ ++ tt] = cur;
+    }
+
+    for (int i = 1, j = 1; i <= tt; i ++ )
+    {
+        while (j <= stk[i].end)
+            ans[j ++ ] = v[stk[i].root];
+    }
+
+    LL res = 0;
+    for (int i = 1; i <= n; i ++ ) res += abs(v[i] - ans[i]);
+    printf("%lld\n", res);
+    for (int i = 1; i <= n; i ++ )
+        printf("%d ", ans[i] + i);
+
+    return 0;
+}
