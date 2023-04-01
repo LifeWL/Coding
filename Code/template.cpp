e3e72fab29283bfd134e3307062825a58b816137
魏煊 <49360064+W-xuan@users.noreply.github.com>
2023-04-02 03:32:07 +0800

    Update template.cpp

--- Code/template.cpp
+++ Code/template.cpp
@@ -8289,3 +8289,54 @@ int main()
     printf("%d\n", f[n][m]);
     return 0;
 }
+
+//打表
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 1010, M = 1010;
+
+int n;
+int w[6][6] = {
+    {1, 0, 1, 1, 0, 0},
+    {0, 1, 0, 0, 1, 0},
+    {0, 1, 0, 0, 1, 0},
+    {0, 1, 0, 0, 1, 0},
+    {1, 0, 1, 1, 0, 1},
+    {0, 0, 0, 0, 1, 0},
+};
+int f[N][6][M];
+
+void add(int a[], int b[])
+{
+    for (int i = 0, t = 0; i < M; i ++ )
+    {
+        t += a[i] + b[i];
+        a[i] = t % 10;
+        t /= 10;
+    }
+}
+
+int main()
+{
+    cin >> n;
+    f[1][1][0] = f[1][4][0] = 1;
+    for (int i = 2; i < n; i ++ )
+        for (int j = 0; j < 6; j ++ )
+            for (int k = 0; k < 6; k ++ )
+                if (w[k][j])
+                    add(f[i][j], f[i - 1][k]);
+    int res[M] = {0};
+    add(res, f[n - 1][0]), add(res,f[n - 1][4]);
+    add(res, res);
+
+    int k = M - 1;
+    while (k > 0 && !res[k]) k -- ;
+    for (int i = k; i >= 0; i -- ) cout << res[i];
+    cout << endl;
+
+    return 0;
+}
