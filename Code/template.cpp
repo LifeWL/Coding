1b3b1916c3952dcd756ad4cafa35e6451a906e90
魏煊 <49360064+W-xuan@users.noreply.github.com>
2023-04-13 01:37:22 +0800

    Update template.cpp

--- Code/template.cpp
+++ Code/template.cpp
@@ -8476,3 +8476,57 @@ int main()
     }
     return 0;
 }
+
+//时态同步
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long LL;
+const int N = 500010, M = N * 2;
+
+int n, root;
+int h[N], e[M], w[M], ne[M], idx;
+LL d[N], ans;
+
+void add(int a, int b, int c)
+{
+    e[idx] = b, w[idx] = c, ne[idx] = h[a], h[a] = idx ++ ;
+}
+
+void dfs(int u, int father)
+{
+    for (int i = h[u]; ~i; i = ne[i])
+    {
+        int j = e[i];
+        if (j == father) continue;
+        dfs(j, u);
+        d[u] = max(d[u], d[j] + w[i]);
+    }
+
+    for (int i = h[u]; ~i; i = ne[i])
+    {
+        int j = e[i];
+        if (j == father) continue;
+        ans += d[u] - (d[j] + w[i]);
+    }
+}
+
+int main()
+{
+    scanf("%d%d", &n, &root);
+    memset(h, -1, sizeof h);
+    for (int i = 0; i < n - 1; i ++ )
+    {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
+        add(a, b, c), add(b, a, c);
+    }
+
+    dfs(root, -1);
+    printf("%lld\n", ans);
+
+    return 0;
+}
