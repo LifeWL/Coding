e8f33eb9039946574a968dedc2b82354fc993aa8
魏煊 <49360064+W-xuan@users.noreply.github.com>
2023-05-02 23:37:10 +0800

    Update template.cpp

--- Code/template.cpp
+++ Code/template.cpp
@@ -9265,3 +9265,44 @@ int main()
 
     return 0;
 }
+
+//线性基
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long LL;
+const int N = 100010;
+
+int n;
+LL a[N];
+
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 0; i < n; i ++ ) scanf("%lld", &a[i]);
+
+    int k = 0;
+    for (int i = 62; i >= 0; i -- )
+    {
+        for (int j = k; j < n; j ++ )
+            if (a[j] >> i & 1)
+            {
+                swap(a[j], a[k]);
+                break;
+            }
+        if (!(a[k] >> i & 1)) continue;
+        for (int j = 0; j < n; j ++ )
+            if (j != k && (a[j] >> i & 1))
+                a[j] ^= a[k];
+        k ++ ;
+        if (k == n) break;
+    }
+
+    LL res = 0;
+    for (int i = 0; i < k; i ++ ) res ^= a[i];
+    printf("%lld\n", res);
+    return 0;
+}
