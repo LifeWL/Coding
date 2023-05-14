f75e9b06193f7c3c85f67a60b234545fede54dd0
魏煊 <49360064+W-xuan@users.noreply.github.com>
2023-05-14 23:26:23 +0800

    Update template.cpp

--- Code/template.cpp
+++ Code/template.cpp
@@ -9929,3 +9929,138 @@ int main()
 
     return 0 ;
 }
+
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+const int N = 600, M = 107, INF = 0x3f3f3f3f;
+
+int n, m;
+int g[110][10], q[2][N], cnt[2];
+int h[2][M], v[2][M];
+
+int max(int x, int y)
+{
+    if (x > y) return x;
+    return y;
+}
+
+int find(int cur, int x)
+{
+    int t = x % M;
+    while (h[cur][t] != -1 && h[cur][t] != x)
+        if ( ++ t == M)
+            t = 0;
+    return t;
+}
+
+int get(int state, int k)
+{
+    return state >> k * 2 & 3;
+}
+
+int set(int k, int v)
+{
+    return v * (1 << k * 2);
+}
+
+void insert(int cur, int state, int w)
+{
+    int t = find(cur, state);
+    if (h[cur][t] == -1)
+    {
+        h[cur][t] = state, v[cur][t] = w;
+        q[cur][ ++ cnt[cur]] = t;
+    }
+    else v[cur][t] = max(v[cur][t], w);
+}
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i ++ )
+        for (int j = 1; j <= m; j ++ )
+            scanf("%d", &g[i][j]);
+
+    int res = -INF;
+    memset(h, -1, sizeof h);
+    int cur = 0;
+    insert(cur, 0, 0);
+    for (int i = 1; i <= n; i ++ )
+    {
+        for (int j = 1; j <= cnt[cur]; j ++ )
+            h[cur][q[cur][j]] <<= 2;
+        for (int j = 1; j <= m; j ++ )
+        {
+            int last = cur;
+            cur ^= 1, cnt[cur] = 0;
+            memset(h[cur], -1, sizeof h[cur]);
+            for (int k = 1; k <= cnt[last]; k ++ )
+            {
+                int state = h[last][q[last][k]], w = v[last][q[last][k]];
+                int x = get(state, j - 1), y = get(state, j);
+                if (!x && !y)
+                {
+                    insert(cur, state, w);
+                    if (i < n && j < m) insert(cur, state + set(j - 1, 1) + set(j, 2), w + g[i][j]);
+                }
+                else if (!x && y)
+                {
+                    if (i < n) insert(cur, state - set(j, y) + set(j - 1, y), w + g[i][j]);
+                    if (j < m) insert(cur, state, w + g[i][j]);
+                }
+                else if (x && !y)
+                {
+                    if (i < n) insert(cur, state, w + g[i][j]);
+                    if (j < m) insert(cur, state - set(j - 1, x) + set(j, x), w + g[i][j]);
+                }
+                else if (x == 1 && y == 1)
+                {
+                    for (int u = j + 1, s = 1;; u ++ )
+                    {
+                        int z = get(state, u);
+                        if (z == 1) s ++ ;
+                        else if (z == 2)
+                        {
+                            if ( -- s == 0)
+                            {
+                                insert(cur, state - set(j - 1, x) - set(j, y) - set(u, 1), w + g[i][j]);
+                                break;
+                            }
+                        }
+                    }
+                }
+                else if (x == 2 && y == 2)
+                {
+                    for (int u = j - 2, s = 1;; u -- )
+                    {
+                        int z = get(state, u);
+                        if (z == 2) s ++ ;
+                        else if (z == 1)
+                        {
+                            if ( -- s == 0)
+                            {
+                                insert(cur, state - set(j - 1, x) - set(j, y) + set(u, 1), w + g[i][j]);
+                                break;
+                            }
+                        }
+                    }
+                }
+                else if (x == 2 && y == 1)
+                {
+                    insert(cur, state - set(j - 1, x) - set(j, y), w + g[i][j]);
+                }
+                else
+                {
+                    if (state == set(j - 1, x) + set(j, y))
+                        res = max(res, w + g[i][j]);
+                }
+            }
+        }
+    }
+
+    printf("%d\n", res);
+    return 0;
+}
